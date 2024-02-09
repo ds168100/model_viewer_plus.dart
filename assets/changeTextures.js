0bce0f21d08293028aad08a9683ff878557de2ad
David Seijas <ds168100@gmail.com>
2024-02-08 22:45:59 -0600

    Add Change Texture Script

---
+++ assets/changeTextures.js
@@ -0,0 +1,27 @@
+let material;
+let saveTexture;
+let input;
+
+      const createAndApplyTexture = async (channel, file) => {
+        const texture = await modelViewerTexture.createTexture(file);
+        if (channel.includes('base') || channel.includes('metallic')) {
+          material.pbrMetallicRoughness[channel].setTexture(texture);
+        } else {
+          material[channel].setTexture(texture);
+        }
+      }
+
+      function changeTexture(changeValue){
+      if(changeValue.includes('Default')){
+        material.pbrMetallicRoughness['baseColorTexture'].setTexture(saveTexture);
+      }else{
+        createAndApplyTexture('baseColorTexture', changeValue);
+       }
+      }
+
+const modelViewerTexture = document.querySelector("model-viewer#model");
+    modelViewerTexture.addEventListener("load", () => {
+     material = modelViewerTexture.model.materials[0];
+     saveTexture = material.pbrMetallicRoughness['baseColorTexture'].texture;
+
+});
